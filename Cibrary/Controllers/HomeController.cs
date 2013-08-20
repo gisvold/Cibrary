d6dbbbca12010363f8f285f21c27b5ca7a3d5af4
Foksware <xinchen_1989@126.com>
2013-08-20 15:09:30 +0200

    Liten endring. FJernet punktum

--- Cibrary/Controllers/HomeController.cs
+++ Cibrary/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Cibrary.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page";
 
             return View();
         }
