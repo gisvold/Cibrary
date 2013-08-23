286adbe8f2560e1cb4dbc7e06877e001e35dbec2
thoresenhaug <thoresen.haug@gmail.com>
2013-08-23 12:21:05 +0200

    Liten fix, som gjør at man kan levere på Loan View

--- Cibrary/Controllers/LoanController.cs
+++ Cibrary/Controllers/LoanController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
 using Cibrary.Models;
+using Microsoft.AspNet.Identity;
 
 namespace Cibrary.Controllers
 {
@@ -18,7 +19,19 @@ namespace Cibrary.Controllers
             var loansNotDelivered = db.Loans.Where(l => l.TimeDelievered == null);
             return View(loansNotDelivered.ToList());
         }
+        public ActionResult DeliverBook(Int32 id)
+        {
+            String userId = User.Identity.GetUserId();
+            Loan loanToDeliver = db.Loans.Find(id, userId);
+            loanToDeliver.TimeDelievered = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
 
         
     }
+    
 }
